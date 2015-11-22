252e17f3a4a367870240baa82f713275def04fc2
MUsama <MUsama@Muhammad-PC>
2015-11-22 21:09:44 +0500

    nanna

--- resources/views/front/watch/_footerScripts.blade.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-                 videojs( 'videoholder', { 
-                  plugins : { 
-                  resolutionSelector: {default_res:'360p'},
-          persistVolume:{
-            namespace: 'persist-Volume'
-          } } });
-  </script>
--- resources/views/front/watch/_headerIncludes.blade.php
+++
@@ -1,7 +0,0 @@
-
-<link rel="stylesheet" href="assets/video-js/video-jsd5f7.css?ver=2.0"/>
-<script type="text/javascript" src="assets/video-js/videod5f7.js?ver=2.0"></script>
-<script type="text/javascript" src="assets/video-js/video-plugins.js"></script>
-
-<script>videojs.options.flash.swf = "assets/video-js/video-js.swf";</script>
-        
